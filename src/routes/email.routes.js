240e7df3a3a6848974a36f82e3bd8b9408990056
MateoBernal06 <133398097+MateoBernal06@users.noreply.github.com>
2025-12-14 10:37:50 -0500

    Arreglo envio de correos electronicos

--- src/routes/email.routes.js
+++ src/routes/email.routes.js
@@ -1,7 +1,7 @@
 
-const app = require('express')
+const express = require('express')
 const {sendMsg} = require('../controller/email.controller.js');
-const routes = app()
+const routes = express.Router()
 
 routes.post('/enviar-email', sendMsg)
 module.exports = routes
